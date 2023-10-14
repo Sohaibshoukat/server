13f67f8bd7872edf2b187706417f5c9253a9b2b5
Sohaib Shoukat <sohaibshoukat94@gmail.com>
2023-10-15 02:39:54 +0500

    push

---
+++ products.js
@@ -0,0 +1,31 @@
+const products = [
+  {
+    id: 1,
+    name: "Rose Gold Brush",
+    brand: "Gold Push",
+    desc: "7 year Time Stamp",
+    price: 50,
+    image:
+      "RoseGoldBrush",
+  },
+  {
+    id: 2,
+    name: "World Radio Invest",
+    brand: "Radio Push",
+    desc: "5 year Time Stamp",
+    price: 100,
+    image:
+      "WorldRadio"
+  },
+  {
+    id: 3,
+    name: "Studio Barik",
+    brand: "Barik Record",
+    desc: "10 years Time Stamp",
+    price: 20,
+    image:
+      "StudioBarik",
+  },
+];
+
+module.exports = products;
